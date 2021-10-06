8ac1d34f99c7e1aa55001cfb87172887383fe64c
Ben <benjaminjsaddler@gmail.com>
2021-10-07 12:29:46 +1300

    Enemy Script Start

--- Assets/Enemy.cs
+++ Assets/Enemy.cs
@@ -4,10 +4,13 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    public GameObject planet;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        planet = GameObject.Find("Planet");
+        transform.LookAt(planet.transform, Vector3.back);
     }
 
     // Update is called once per frame
