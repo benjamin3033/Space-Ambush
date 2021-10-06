8434570464af240c0780aea5fdc5a0906ed82a1f
Ben <benjaminjsaddler@gmail.com>
2021-10-06 14:53:34 +1300

    Project Start
    Initial gameplay

---
+++ Assets/GameManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+
+    GameObject player = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Instantiate(player);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/PlayerController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public GameObject planet = null;
+    public GameObject bullet = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        gameObject.transform.RotateAround(planet.transform.position, Vector3.forward, 50 * Time.deltaTime);
+        BulletFiring();
+    }
+
+    private void BulletFiring()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            GameObject clone;
+            Rigidbody2D cloneRb;
+            clone = Instantiate(bullet, gameObject.transform.position, gameObject.transform.rotation);
+            cloneRb = clone.GetComponent<Rigidbody2D>();
+            cloneRb.velocity = transform.TransformDirection(Vector2.up * 10);
+        }
+    } 
+}
