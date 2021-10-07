377282b0aedd30b80c3b34402c47b3d0dfc5a796
Ben <benjaminjsaddler@gmail.com>
2021-10-07 13:13:45 +1300

    Start of enemy spawning

--- Assets/Enemy.cs
+++ Assets/Enemy.cs
@@ -5,17 +5,31 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
     public GameObject planet;
+    public float speed = 0.5f;
 
     // Start is called before the first frame update
     void Start()
     {
         planet = GameObject.Find("Planet");
-        transform.LookAt(planet.transform, Vector3.back);
+        RotateTowards(planet.transform.position);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        MoveTowards(planet.transform.position);
+    }
+
+    void MoveTowards(Vector2 target)
+    {
+        transform.position = Vector2.MoveTowards(transform.position, target, speed * Time.deltaTime);
+    }
+
+    void RotateTowards(Vector2 target)
+    {
+        Vector2 direction = (target - (Vector2)transform.position).normalized;
+        var angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        var offset = 90f;
+        transform.rotation = Quaternion.Euler(Vector3.back * (angle + offset));
     }
 }
---
+++ Assets/GameManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+
+    public Camera mainCamera = null;
+    public GameObject enemy = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mainCamera.orthographicSize = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CameraZoom();
+    }
+    
+    void CameraZoom()
+    {
+        if(mainCamera.orthographicSize < 5)
+        {
+            mainCamera.orthographicSize += Time.deltaTime;
+        }
+        else
+        {
+            StartCoroutine("SpawnEnemy");
+        }
+    }
+
+    IEnumerator SpawnEnemy()
+    {
+        Debug.Log("Spawned Enemy");
+        yield return new WaitForSeconds(5f);
+    }
+}
